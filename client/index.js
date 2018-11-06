29f21f6094c556df12c0431d3f57d100704072ec
Nishant <nishuprajapati92@gmail.com>
2018-11-06 16:02:11 +0530

    changes as per requested

--- client/index.js
+++ client/index.js
@@ -1,11 +1,26 @@
 import $ from 'jquery';
 
-$(document).ready(function(){
+$(document).ready(function () {
 
+    $(".menuitem").click(function () {
+        $(".menu").toggleClass("visible");
+    });
 
-$(".menuitem").click(function(){
-    $(".menu").toggleClass("visible");
-})
-    
+    $(".menuitem > .menu > li").each(function(){
+        console.log("value");
+    })
 
+    showinput(document);
+
+    $(window).on('resize', function () {
+        showinput(this);
+    });
+
+    function showinput(content) {
+        if ($(content).width() < 700) {
+            $(".search").click(function () {
+                $(".search input").toggleClass("visible");
+            })
+        }
+    }
 });
